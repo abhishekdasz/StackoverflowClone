06cf687f3dbd70ab04006112c2d4209abb03b7a8
Abhishek <abhishekdaszy@gmail.com>
2024-01-15 23:56:53 +0530

    creating the hero-section

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Questions from './Questions'
+
+const HeroSection = () => {
+  return (
+    <div className='hero-section'>
+      <Questions/>
+    </div>
+  )
+}
+
+export default HeroSection
