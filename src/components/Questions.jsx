76ab03bbcef8cee45e85109596ec07c1be3f731d
Abhishek <abhishekdaszy@gmail.com>
2024-01-16 01:28:15 +0530

    creating the hero-section

--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import authorImg from '../assets/author.jpg'
 
 const Questions = () => {
   return (
     <div className='questions-section'>
         <div className="questions-container">
+
+
+
             <div className="questions-heading">
                 <div className="question-title">
                     <h3> How to scroll (move screen up/down while cursor remains fixed at same screen position) in Vim </h3>
@@ -18,7 +22,7 @@ const Questions = () => {
 
                 <div className="question-author-details">
                     <div className="author-desc">
-                        <img src="/author.jpg" alt="a" />
+                        <img src={authorImg} alt="authorImg" />
                         <p> Richardo Rocha </p>
                         <div>
                             <p> 9038 </p>
@@ -28,6 +32,15 @@ const Questions = () => {
                         <pre> Asked 2years 1month ago | Active 21 days ago | Viewed 123k times </pre>
                     </div>
                 </div>
+            </div>
+
+
+            <div className="questions-description">
+                <p> Inside of visual studio code, I'm trying to execute a script.bat from the command line, but I'm getting the following error. </p>
+                <div className="compiler">
+                    <p> File C:\Theses_Repo\train-cnn\environment\Scripts\activate.psl cannot be loaded because running scripts is disabled on this system. </p>
+                </div>
+                <p> After reading this I tried to run the visual studio code in administrator mode, thinking that the problem was a matter of privileges. But the error is throwing anyway. </p>
             </div>
         </div>
     </div>
