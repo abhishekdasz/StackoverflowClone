a6c628510c1651227e17b7981d524ee8de76eef7
Abhishek <abhishekdaszy@gmail.com>
2024-01-16 00:25:56 +0530

    creating the hero-section

--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -4,17 +4,23 @@ const Questions = () => {
   return (
     <div className='questions-section'>
         <div className="questions-container">
-            <div className="question-title">
-                <h3> How to scroll (move screen up/down while cursor remains fixed at same screen position) in Vim </h3>
-            </div>
-            <div className="question-author-details">
-                <div className="a">
-                    <img src="/author.jpg" alt="a" />
-                    <p> Richardo Rocha </p>
-                    <p> 9038 </p>
+            <div className="questions-heading">
+                <div className="question-title">
+                    <h3> How to scroll (move screen up/down while cursor remains fixed at same screen position) in Vim </h3>
+                    <p> - 103 + </p>
                 </div>
-                <div className="b">
-                    <p> Asked 2years 1month ago | Active 21 days ago | Viewed 123k times </p>
+
+                <div className="question-author-details">
+                    <div className="author-desc">
+                        <img src="/author.jpg" alt="a" />
+                        <p> Richardo Rocha </p>
+                        <div>
+                            <p> 9038 </p>
+                        </div>
+                    </div>
+                    <div className="question-asked-time">
+                        <p> Asked 2years 1month ago | Active 21 days ago | Viewed 123k times </p>
+                    </div>
                 </div>
             </div>
         </div>
