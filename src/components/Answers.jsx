14dc0b76b11bf0e94765ace9b8fbf31ac5252181
Abhishek <abhishekdaszy@gmail.com>
2024-01-16 11:45:59 +0530

    creating the answer-section

---
+++ src/components/Answers.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import './Answers.scss'
+
+const Answers = () => {
+  return (
+    <div className='answers-section'>
+        <div className="answers-container">
+            <div className="ans-nav">
+                <p> 20 Answers </p>
+                <div className="nav">
+                    <p> Votes </p>
+                    <p> Oldest </p>
+                    <p> Active </p>
+                </div>
+            </div>
+
+            <div className="ans-author-details">
+                <div className="auth-det">
+                    <p> Author:  </p>
+                    <p> javed (308) </p>
+                    <p> Answered: Dec 9 '19 at 8:20 </p>
+                </div>
+                <div className="btn">
+                    <p id='minus'> - </p>
+                    <p id='num'> 103 </p>
+                    <p id='plus'> + </p>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Answers
--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Questions from './Questions'
+import Answers from './Answers'
 
 const HeroSection = () => {
   return (
     <div className='hero-section'>
       <Questions/>
+      <br />
+      <Answers/>
     </div>
   )
 }
