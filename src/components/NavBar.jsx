5336a16be69b4d977474ab1152d7eb40f32f3fb1
Abhishek <abhishekdaszy@gmail.com>
2024-01-16 16:12:03 +0530

    creating the navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
+import './Navbar.scss'
+import stackLogo from '../assets/logo.svg'
+import { AiOutlineSearch, AiOutlineMenu } from "react-icons/ai";
 
 const NavBar = () => {
   return (
     <div className='navbar'>
-        NavBar
+        <section className="navbar_wrapper">
+        <div className="navbar">
+          <div className="logo">
+            <img src={stackLogo} alt="stack-overflow-logo" />
+          </div>
+          <div className="search_wrapper">
+            <AiOutlineSearch className="search_icon" size={20} />
+            <input
+              className="search_input"
+              placeholder="Search"
+              type="text"
+              name=""
+              id=""
+            />
+          </div>
+          <div className="menu">
+            <a href="/">About</a>
+            <a href="/">Product</a>
+            <a href="/">For teams</a>
+          </div>
+          <AiOutlineMenu className="menu_bar" size={20}/>
+        </div>
+      </section>
     </div>
   )
 }
